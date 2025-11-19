51e025eb693c1a5430656e7d983839154dee8f06
SV <25300520+SaintVamp@users.noreply.github.com>
2025-11-19 16:09:42 +0800

    up

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-02-23 20:11:27"
-ts = 1740312687
+ver = "2025-11-19 14:27:44"
+ts = 1763533664
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
