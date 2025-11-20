be169bcd481b5721058e460d968f364922ceab1a
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-11-20 03:09:36 +0000

    auto package test

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-11-19 14:11:57"
-ts = 1763561517
+ver = "2025-11-20 03:09:35"
+ts = 1763608175
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
